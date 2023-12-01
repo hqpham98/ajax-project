667049dc4b38bffe83d003da79a9bc79695f8963
Henry Pham <hqpham98@gmail.com>
2023-12-01 00:26:50 +0000

    Added delay before search is queried #2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const targetURL = 'https://api.rawg.io/api/games';
 const key = 'c76172b8a9d1457998e7ae8127840d5d';
+let timeoutID = null;
 const $search = document.querySelector('#search-bar');
 const $searchForm = document.querySelector('#search-form');
 
@@ -11,7 +12,15 @@ async function getSearchResults(query) {
 }
 
 async function handleInput(event) {
-  data.results = await getSearchResults(event.target.value);
+  if (event.target.value.trim() !== '') {
+    async function test() {
+      console.log('started');
+      data.results = await getSearchResults(event.target.value);
+      console.log(data.results);
+    }
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(test, 750);
+  }
 }
 
 async function handleSubmit(event) {
